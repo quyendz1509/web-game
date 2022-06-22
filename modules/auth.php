9478f048f12c2fa1f6bd7254e9dcca25ef108ceb
quyendz1509 <quyendz1509@gmail.com>
2022-06-23 01:40:02 +0700

    update get ip

--- modules/auth.php
+++ modules/auth.php
@@ -75,96 +75,89 @@ class authClass extends DATABASE
 
              }else{
               $res = 'die';
-             }
+            }
 
-             return $res;
-           }
-           function checkCookieId($id_hash,$_ID_USER_NUMBER){
+            return $res;
+          }
+          function checkCookieId($id_hash,$_ID_USER_NUMBER){
 		// Lấy thông tin tất cả member (Củ chuối thế nhờ :( )
-             $sql='SELECT * FROM `users`';
+           $sql='SELECT * FROM `users`';
 		// kết quả là gì ? 
-             $kq = $this->pdo_query($sql);
+           $kq = $this->pdo_query($sql);
 		// Bắt đầu
-             $test = '';
-             foreach ($kq as $key => $value) {
-              if (md5( $value['ID'] + $_ID_USER_NUMBER.$value['idnumber'].$value['passwd2']  ) == $id_hash) {
-               $test = $value['ID'];
-               break;
-             }else{
-               $test = false;
+           $test = '';
+           foreach ($kq as $key => $value) {
+            if (md5( $value['ID'] + $_ID_USER_NUMBER.$value['idnumber'].$value['passwd2']  ) == $id_hash) {
+             $test = $value['ID'];
+             break;
+           }else{
+             $test = false;
 
-             }
            }
-           return $test;
          }
+         return $test;
+       }
 	// check email
-         function checkEmail($email){
-           $sql='SELECT * FROM `users` WHERE `email` = ?';
-           return $this->pdo_query_one($sql,$email);
-         }
+       function checkEmail($email){
+         $sql='SELECT * FROM `users` WHERE `email` = ?';
+         return $this->pdo_query_one($sql,$email);
+       }
 	// insert new pass
-         function insertSecurityPassword($password,$id){
-           $sql='UPDATE `users` SET `answer` = ? WHERE `ID` =? ';
-           $this->pdo_execute($sql,$password,$id);
+       function insertSecurityPassword($password,$id){
+         $sql='UPDATE `users` SET `answer` = ? WHERE `ID` =? ';
+         $this->pdo_execute($sql,$password,$id);
 		// get info user by id
-         }
-         function userNameInfomationId($id){
-           $sql='SELECT * FROM `users` WHERE `ID` = ?';
-           return $this->pdo_query_one($sql,$id);
-         }
-         function getListUserAll(){
-           return $this->pdo_query('SELECT * FROM `users` ORDER BY `ID` DESC');
-         }
+       }
+       function userNameInfomationId($id){
+         $sql='SELECT * FROM `users` WHERE `ID` = ?';
+         return $this->pdo_query_one($sql,$id);
+       }
+       function getListUserAll(){
+         return $this->pdo_query('SELECT * FROM `users` ORDER BY `ID` DESC');
+       }
 	// function update ip 
-         function updateIp($ip,$id){
-           $sql='UPDATE `users` SET `passwd2` = ?, `thedangky` = ? WHERE `id` = ?';
-           $this->pdo_execute($sql,$ip,$ip,$id);
-         }
+       function updateIp($ip,$id){
+         $sql='UPDATE `users` SET `passwd2` = ?, `thedangky` = ? WHERE `id` = ?';
+         $this->pdo_execute($sql,$ip,$ip,$id);
+       }
 	// get all info user 
-         function getListUser(){
-           return $this->pdo_query_values('SELECT IFNULL(MAX(id), 16) + 16 FROM `users`');
-         }
+       function getListUser(){
+         return $this->pdo_query_values('SELECT IFNULL(MAX(id), 16) + 16 FROM `users`');
+       }
 	// Get info user by username
-         function getInfoByUserName($username){
-           $sql='SELECT * FROM `users` WHERE `name` = ?';
-           return $this->pdo_query_one($sql,$username);
-         }
+       function getInfoByUserName($username){
+         $sql='SELECT * FROM `users` WHERE `name` = ?';
+         return $this->pdo_query_one($sql,$username);
+       }
 	// function curl
-         function curlGet($url){
-           $ch = curl_init();
-           curl_setopt($ch, CURLOPT_URL, $url);
-           curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-           $result = curl_exec($ch);
-           curl_close($ch);
-           return $result;
+       function curlGet($url){
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $url);
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         $result = curl_exec($ch);
+         curl_close($ch);
+         return $result;
 
-         }
+       }
 	// Tạo người dùng
-         function makeMember($username,$salt,$password,$email,$phone,$coderand,$gender,$ip,$cretime,$id){
-           $sql='INSERT INTO `users`(`name`,`passwd`,`Prompt`,`answer`,`truename`,`idnumber`,`email`,`mobilenumber`,`province`,`city`,`phonenumber`,`address`,`postalcode`,`gender`,`qq`,`passwd2`,`chuyenkhoan`,`napthe`,`hotro1`,`hotro2`,`thedangky`,`creatime`,`id`) VALUES(?,?,0,0,0,?,?,?,0,0,0,0,?,?,0,?,0,0,0,0,?,?,?)';
-           $this->pdo_execute($sql,$username,$salt,$password,$email,$phone,$coderand,$gender,$ip,$ip,$cretime,$id);
-         }
+       function makeMember($username,$salt,$password,$email,$phone,$coderand,$gender,$ip,$cretime,$id){
+         $sql='INSERT INTO `users`(`name`,`passwd`,`Prompt`,`answer`,`truename`,`idnumber`,`email`,`mobilenumber`,`province`,`city`,`phonenumber`,`address`,`postalcode`,`gender`,`qq`,`passwd2`,`chuyenkhoan`,`napthe`,`hotro1`,`hotro2`,`thedangky`,`creatime`,`id`) VALUES(?,?,0,0,0,?,?,?,0,0,0,0,?,?,0,?,0,0,0,0,?,?,?)';
+         $this->pdo_execute($sql,$username,$salt,$password,$email,$phone,$coderand,$gender,$ip,$ip,$cretime,$id);
+       }
 	// Function to get the client IP address
-         function get_client_ip() {
-           $ipaddress = '';
-           if (getenv('HTTP_CLIENT_IP'))
-            $ipaddress = getenv('HTTP_CLIENT_IP');
-          else if(getenv('HTTP_X_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-          else if(getenv('HTTP_X_FORWARDED'))
-            $ipaddress = getenv('HTTP_X_FORWARDED');
-          else if(getenv('HTTP_FORWARDED_FOR'))
-            $ipaddress = getenv('HTTP_FORWARDED_FOR');
-          else if(getenv('HTTP_FORWARDED'))
-            $ipaddress = getenv('HTTP_FORWARDED');
-          else if(getenv('REMOTE_ADDR'))
-            $ipaddress = getenv('REMOTE_ADDR');
-          else
-            $ipaddress = 'UNKNOWN';
-          return $ipaddress;
-        }
+       function get_client_ip() {
+         $url = 'https://api64.ipify.org?format=json';
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $url);
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         $result = curl_exec($ch);
+         curl_close($ch);
+         $ipaddress = json_decode($result);
+         return $ipaddress->ip;
+       }
 
-      }
+     }
 
-    ?>
+   ?>
--- test.php
+++ test.php
@@ -1,27 +1,27 @@
 <?php 
-require 'modules/database.php';
-require 'modules/main.php';
-$main = new mainClass();
+// require 'modules/database.php';
+// require 'modules/main.php';
+// $main = new mainClass();
 
-$rand  = $main->vongquay();
- // 20/10/100/50/80/0
-// thuật toán viper của quyền
-$mangmoi = [];
+// $rand  = $main->vongquay();
+//  // 20/10/100/50/80/0
+// // thuật toán viper của quyền
+// $mangmoi = [];
 
-foreach ($rand as $key => $value) {
-	if ( 100 - $value['percent'] <= 50 ) {
-		$mangmoi[] = $value['name'];
-	}
-}
+// foreach ($rand as $key => $value) {
+// 	if ( 100 - $value['percent'] <= 50 ) {
+// 		$mangmoi[] = $value['name'];
+// 	}
+// }
 
-//  // mảng sẽ ra giá trị là : 2/3/4/5 tức là 0/50/20/10
-$last = [];
-for ($i=0; $i < count($mangmoi); $i++) { 
-	$last[] = $mangmoi[$i];
-}
-$rand_number = mt_rand(0, count($last) - 1);
-echo $last[$rand_number];
-// 2 giá trị cuối sẽ là : 0 và 20 
+// //  // mảng sẽ ra giá trị là : 2/3/4/5 tức là 0/50/20/10
+// $last = [];
+// for ($i=0; $i < count($mangmoi); $i++) { 
+// 	$last[] = $mangmoi[$i];
+// }
+// $rand_number = mt_rand(0, count($last) - 1);
+// echo $last[$rand_number];
+// // 2 giá trị cuối sẽ là : 0 và 20 
 
 
 ?>
